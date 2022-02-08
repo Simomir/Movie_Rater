d9465d72fcf63e3264334d0e2e5804922fb762dd
Simeon Shopov <simeon.t.shopov@gmail.com>
2022-02-08 15:57:27 +0200

    Added api app

--- Movie_Rater/settings.py
+++ Movie_Rater/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG', False)
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
     # 3rd party apps
     'rest_framework',
+    'rest_framework.authtoken',
 
     # own apps
 ]
