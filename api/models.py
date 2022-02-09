67f7e0c679a27cae978b64a1ba407029066a7359
Simeon Shopov <simeon.t.shopov@gmail.com>
2022-02-09 12:16:54 +0200

    Model methods for number of ratings and average rating

--- api/models.py
+++ api/models.py
@@ -1,11 +1,25 @@
 from django.contrib.auth.models import User
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
+from statistics import mean
 
 
 class Movie(models.Model):
     title = models.CharField(max_length=32)
     description = models.TextField(max_length=360)
+
+    def _get_ratings(self):
+        return Rating.objects.filter(movie=self)
+
+    def number_of_ratings(self):
+        return len(self._get_ratings())
+
+    def avg_rating(self):
+        ratings = self._get_ratings()
+        if len(ratings) > 0:
+            return mean([int(rating.stars) for rating in ratings])
+        else:
+            return 0
 
 
 class Rating(models.Model):
